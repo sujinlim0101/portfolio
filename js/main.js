2d87a6969fafe8fa7cda4b4bf8aa585aa1670517
sujinlim0101 <60640614+sujinlim0101@users.noreply.github.com>
2020-08-13 20:34:29 +0900

    Fix indent mistakes, Add comments in js and Remove unneed codes

--- js/main.js
+++ js/main.js
@@ -41,6 +41,8 @@
             }
         },
         {//1  sticky가 아니지만 heigth를 비율로 조정하기 위해 씀
+        //캔버스가 하얀색이고 양옆으로 여백이 남는경우) 스크롤이 빠르게 될 때 transition효과가 나타날때 양옆에 검은 테두리가 생김. 
+        //그것을 방지하기 위해 창 사이즈만큼 section의 높이로 주고, 완화하기 위한 조치로서 height num을 1로 줌.
             type:'sticky', 
             heightNum:1,
             scrollHeight:0,
@@ -50,7 +52,7 @@
             values: {
             }
         },
-        {//2
+        {//2 normal은 자신이 가진 hegiht만큼 가지기 때문에 조정하지 않음.
             type:'normal',
             objs: {
                 container: document.querySelector('#scroll-section-2'),
@@ -116,6 +118,7 @@
         },
         
     ]
+    //nav의 sticky와 투명도 조절.
     function checkNav() {
         if (yOffset > 44) {
 			document.body.classList.add('nav-sticky');
@@ -151,6 +154,8 @@
         }
     }
     function setLayout() {
+    
+        //section의 height를 셋팅.
         for (let i =0; i<sceneInfo.length; i++) {
             if (sceneInfo[i].type === 'normal'){
                 sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight;
@@ -169,8 +174,10 @@
                 break;
             }
         }
+        //세로 기준에 맞추어 캔버스 크기를 조절함 
         document.body.setAttribute('id',`show-scene-${currentScene}`);
         const heightRatio = document.documentElement.clientHeight / 1080;
+        //캔버스가 가운데로 조절되기 위해 맞춰주는 값
         sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
         sceneInfo[5].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
 
@@ -186,12 +193,12 @@
 
         switch(currentScene) {
             case 0:
-                //섹션0으로 들어올 때 배경색 화이트 빼기
+                //섹션0으로 들어올 때 배경색 화이트 빼기 
+                //너무 빨리 스크롤 경우에 black-ani가 빠지지 않을 수 있기 떄문에 모든 section에 추가해줌.
                 transition_conatiner.removeAttribute('class','black-ani')
                 let sequence = Math.round(calcValues(values.imgaeSequence, currentYOffset));
                 objs.context.drawImage(objs.videoImages[sequence],0,0);
-                // console.log('0 play');
-                
+                //opacity in과 opacity out이 겹치는 구간이 있기 때문에 0.2와 0.25의 중간값으로 서로 겹치는 영역이 없게 처리.
                 if (scrollRatio <= 0.22) {
                     // in
                     objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
@@ -232,7 +239,8 @@
                     objs.messageD.style.transform = `translate3d(0, ${calcValues(values.messageD_translateY_out, currentYOffset)}%, 0)`;
                 }
                 break;
-            case 1:                
+            case 1:
+                //섹션 1-3까지 영역의 black-ani 추가      
                 transition_conatiner.removeAttribute('class','black-ani')
             case 2:
                 transition_conatiner.setAttribute('class','black-ani')
@@ -246,13 +254,13 @@
                 objs.container.style.background = '#f9f3e4';
 
                 if(scrollRatio >0.8) {
+                    //부드럽게 캔버스로 넘어가기 위해서 하얀색 배경으로 바꿈.
                     objs.container.style.background = 'white';
                 }
                 break;            
             case 5:
                 let sequence2 = Math.round(calcValues(values.imgaeSequence, currentYOffset));
                 objs.context.drawImage(objs.videoImages[sequence2],0,0);
-                transition_conatiner.setAttribute('class','little-yellow')
                 if (scrollRatio <= 0.5) {
                     objs.canvas.style.opacity = calcValues(values.canvas_opacity_in, currentYOffset);
                 } else {
@@ -323,18 +331,22 @@
 
     }
     function scrollLoop() {
+        //씬에 들어왔는지 검사하는 값.
         enterNewScene = false;
         yOffset = window.pageYOffset;
         prevScrollHeight=0; 
 
         for (let i = 0; i < currentScene ; i ++) {
+            //전 섹션의 모든 값을 합침.
             prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
         }
         if(yOffset > prevScrollHeight+ sceneInfo[currentScene].scrollHeight) {
+            //현 스크롤 값이 전 섹션 + 지금 섹션의 값보다 더 값이 큰(+1) 씬으로 감. 
             enterNewScene = true;
             currentScene++;
         }
         if ( yOffset < prevScrollHeight) {
+            //현 스크롤 값이 전 섹션 + 지금 섹션의 값보다 더 값이 작은(-1) 씬으로 감. 
             if( currentScene === 0) { //모바일에서 바운스 되는 것 떄문에 yoffset이 -되어 currentscene이 -되는 것 방지
                 return;
             }
@@ -342,11 +354,13 @@
             currentScene--;
         }
         document.body.setAttribute('id', `show-scene-${currentScene}`);
+        // 씬에 처음으로 들어갔을 때 이상값이 생기는 현상 때문에 이 때 한번 패스하고, return.
         if (enterNewScene) return;
         playAnimation();
     }
 
     window.addEventListener('load', () => {
+        //로드가 끝나면 before load 클래스를 없앰.
         document.body.classList.remove('before-load');
         setLayout();
         sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0],0,0);
@@ -355,14 +369,14 @@
             scrollLoop();
             checkNav();
         } )
-
+        //리사이즈할 떄 setLayout 다시해줘야함.
         window.addEventListener('resize',setLayout);
 
-
+        //모바일에서 가로모드로 전환
         window.addEventListener('orientationchange', () => {
             setTimeout(setLayout, 500);
         });
-
+        //트랜지션효과 때문에 div(.loading)을 바로 없애지 않고 transitionend 이후에 제거.
         document.querySelector('.loading').addEventListener('transitionend', (e) => {
             if(document.querySelector('.loading')) {
                 document.body.removeChild(e.currentTarget);
